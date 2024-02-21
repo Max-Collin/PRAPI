848e8c7aae9d4cf264d1523d184732c038bbad1b
Max-Collin <146649643+Max-Collin@users.noreply.github.com>
2024-02-21 11:22:48 +0000

    finished module

---
+++ Source/PRAPI/Public/Enemy/AI/Tasks/MyBTTask_MoveTo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_MoveTo.h"
+#include "MyBTTask_MoveTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRAPI_API UMyBTTask_MoveTo : public UBTTask_MoveTo
+{
+	GENERATED_BODY()
+	
+};
